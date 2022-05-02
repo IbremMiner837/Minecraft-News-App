4242a9a0c19cba8ff080172d20716c2770cbbe4f
Ibragim Maltsagov <ibragim837.tab@gmail.com>
2022-05-02 20:53:22 +0200

    - Fixed bug with SplashScreen

--- app/src/main/java/com/mcbedrock/minecraftnews/MainActivity.java
+++ app/src/main/java/com/mcbedrock/minecraftnews/MainActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.webkit.WebView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.splashscreen.SplashScreen;
 
 import com.google.android.material.color.DynamicColors;
 import com.mcbedrock.minecraftnews.databinding.ActivityMainBinding;
