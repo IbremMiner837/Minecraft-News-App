c954dcb72ca4dbbc7e4c68e35d6ede1caa05ce57
Ibragim Maltsagov <ibragim837.tab@gmail.com>
2022-05-03 08:22:24 +0200

    - Added BottomSheet - Added base functional for BottomAppBar

--- app/src/main/java/com/mcbedrock/minecraftnews/MainActivity.java
+++ app/src/main/java/com/mcbedrock/minecraftnews/MainActivity.java
@@ -2,16 +2,21 @@ package com.mcbedrock.minecraftnews;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.webkit.WebView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.splashscreen.SplashScreen;
 
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.google.android.material.color.DynamicColors;
 import com.mcbedrock.minecraftnews.databinding.ActivityMainBinding;
 
 import br.tiagohm.markdownview.css.ExternalStyleSheet;
+import br.tiagohm.markdownview.css.InternalStyleSheet;
+import br.tiagohm.markdownview.css.styles.Github;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,7 +32,31 @@ public class MainActivity extends AppCompatActivity {
 
         DynamicColors.applyToActivitiesIfAvailable(getApplication());
 
+        InternalStyleSheet css = new Github();
+        binding.MarkdownView.addStyleSheet(css);
         binding.MarkdownView.loadMarkdownFromUrl("https://raw.githubusercontent.com/JVMFrog/Minecraft-Changeloges/master/English/Minecraft-Bedrock/Realeses/1-18-30.md");
-        binding.MarkdownView.addStyleSheet(ExternalStyleSheet.fromAsset("github.css", null));
+
+        BottomAppBar();
+    }
+
+    public void BottomAppBar() {
+        BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(this);
+        bottomSheetDialog.setContentView(R.layout.bottom_sheet_dialog_menu);
+
+        binding.BottomAppBar.setNavigationOnClickListener(view1 -> bottomSheetDialog.show());
+
+        binding.BottomAppBar.setOnMenuItemClickListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.news:
+                    break;
+
+                case R.id.settings:
+                    break;
+
+                case R.id.about:
+                    break;
+            }
+            return false;
+        });
     }
 }
