8ed36888cad5b6cfb74007d2fe853ff1da9b5ea3
Ibragim Maltsagov <ibragim837.tab@gmail.com>
2022-05-05 16:31:03 +0200

    - Added parse Minecraft News, not working :moyai:

--- app/src/main/java/com/mcbedrock/minecraftnews/API/jsonParseAPI.java
+++ app/src/main/java/com/mcbedrock/minecraftnews/API/jsonParseAPI.java
@@ -1,19 +1,6 @@
 package com.mcbedrock.minecraftnews.API;
 
 public class jsonParseAPI {
-    /*
-    https://code.google.com/p/google-gson/
-    String sURL = "http://freegeoip.net/json/"; //just a string
-    // Connect to the URL using java's native library
-    URL url = new URL(sURL);
-    URLConnection request = url.openConnection();
-    request.connect();
-    // Convert to a JSON object to print data
-    JsonParser jp = new JsonParser(); //from gson
-    JsonElement root = jp.parse(new InputStreamReader((InputStream) request.getContent())); //Convert the input stream to a json element
-    JsonObject rootobj = root.getAsJsonObject(); //May be an array, may be an object.
-    String zipcode = rootobj.get("zip_code").getAsString(); //just grab the zipcode
-     */
 
     /*
     для парса: https://www.minecraft.net/content/minecraft-net/_jcr_content.articles.grid
--- app/src/main/java/com/mcbedrock/minecraftnews/ui/MainActivity.java
+++ app/src/main/java/com/mcbedrock/minecraftnews/ui/MainActivity.java
@@ -11,7 +11,6 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
-import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
@@ -31,17 +30,19 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
-    private List<BaseModel> models;
+    private List<BaseModel> base_models;
+    private List<NewsModel> news_models;
     private ChangelogsAdapter adapter;
-    private String BEDROCK_JSON = "https://raw.githubusercontent.com/JVMFrog/Minecraft-Changeloges/master/English/bedrock-releases.json";
-    private String BETA_JSON = "https://raw.githubusercontent.com/JVMFrog/Minecraft-Changeloges/master/English/bedrock-beta-and-preview.json";
-    private String JAVA_JSON = "https://raw.githubusercontent.com/JVMFrog/Minecraft-Changeloges/master/English/java-realeses.json";
-    private String SNAPSHOT_JSON = "https://raw.githubusercontent.com/JVMFrog/Minecraft-Changeloges/master/English/java-snapshots.json";
+    private MinecraftNewsAdapter minecraftNewsAdapter;
+    private final String NEWS_JSON = "https://www.minecraft.net/content/minecraft-net/_jcr_content.articles.grid";
+    private final String BEDROCK_JSON = "https://raw.githubusercontent.com/JVMFrog/Minecraft-Changeloges/master/English/bedrock-releases.json";
+    private final String BETA_JSON = "https://raw.githubusercontent.com/JVMFrog/Minecraft-Changeloges/master/English/bedrock-beta-and-preview.json";
+    private final String JAVA_JSON = "https://raw.githubusercontent.com/JVMFrog/Minecraft-Changeloges/master/English/java-realeses.json";
+    private final String SNAPSHOT_JSON = "https://raw.githubusercontent.com/JVMFrog/Minecraft-Changeloges/master/English/java-snapshots.json";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,59 +54,67 @@ public class MainActivity extends AppCompatActivity {
 
         DynamicColors.applyToActivitiesIfAvailable(getApplication());
 
-        models = new ArrayList<>();
-
-        //InternalStyleSheet css = new Github();
-        //binding.MarkdownView.addStyleSheet(css);
-        //binding.MarkdownView.loadMarkdownFromUrl("https://raw.githubusercontent.com/JVMFrog/Minecraft-Changeloges/master/English/Minecraft-Bedrock/Realeses/1-18-30.md");
+        base_models = new ArrayList<>();
+        news_models = new ArrayList<>();
 
         BottomAppBar();
-        ExtractJson(BEDROCK_JSON);
+        ParseNews(NEWS_JSON);
     }
 
-    private void ExtractJson(String jsonURL) {
+    private void ParseChangelogs(String jsonURL) {
         RequestQueue queue = Volley.newRequestQueue(this);
-        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, jsonURL, null, new Response.Listener<JSONArray>() {
-            @Override
-            public void onResponse(JSONArray response) {
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, jsonURL, null, response -> {
+            try {
+                for (int i = 0; i < response.length(); i++) {
+                    JSONObject jsonObject = response.getJSONObject(i);
 
-                try {
-                    for (int i = 0; i < response.length(); i++) {
-                        JSONObject jsonObject = response.getJSONObject(i);
+                    BaseModel model = new BaseModel();
+                    /*model.setTitle(jsonObject1.getJSONObject("default_title").getString("title"));
+                    model.setSub_header(jsonObject1.getJSONObject("default_title").getString("sub_header"));
+                    model.setImage_url(jsonObject1.getJSONObject("default_title").getJSONObject("image").getString("image"));*/
 
-                        BaseModel model = new BaseModel();
-                        /*model.setTitle(jsonObject1.getJSONObject("default_title").getString("title"));
-                        model.setSub_header(jsonObject1.getJSONObject("default_title").getString("sub_header"));
-                        model.setImage_url(jsonObject1.getJSONObject("default_title").getJSONObject("image").getString("image"));*/
+                    model.setTitle(jsonObject.getString("title"));
+                    model.setVersion(jsonObject.getString("version"));
+                    model.setUrl_text(jsonObject.getString("changelogURL"));
+                    model.setImage_url(jsonObject.getString("imageURL"));
 
-                        model.setTitle(jsonObject.getString("title"));
-                        model.setVersion(jsonObject.getString("version"));
-                        model.setUrl_text(jsonObject.getString("changelogURL"));
-                        model.setImage_url(jsonObject.getString("imageURL"));
-
-                        models.add(model);
-                    }
-                    binding.recview.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
-                    adapter = new ChangelogsAdapter(getApplicationContext(), models);
-                    binding.recview.setAdapter(adapter);
-                } catch (JSONException e) {
-                    //
+                    base_models.add(model);
                 }
+                binding.recview.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+                adapter = new ChangelogsAdapter(getApplicationContext(), base_models);
+                binding.recview.setAdapter(adapter);
+            } catch (JSONException e) {
+                //
             }
         }, error -> Log.d("tag", "OnErrorResponse" + error.getMessage()));
 
         queue.add(jsonArrayRequest);
+    }
 
-        /*JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, jsonURL, null, new Response.Listener<JSONObject>() {
-            @Override
-            public void onResponse(JSONObject response) {
-                try {
-                    JSONObject object = response.getJSONObject("").getString("");
-                } catch (JSONException e) {
-                    e.printStackTrace();
+    private void ParseNews(String newsURL) {
+        RequestQueue queue = Volley.newRequestQueue(this);
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, newsURL, null, response -> {
+            try {
+                JSONArray jsonArray = response.getJSONArray("article_grid");
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    JSONObject jsonObject1 = jsonArray.getJSONObject(i);
+
+                    NewsModel model = new NewsModel();
+                    model.setTitle(jsonObject1.getJSONObject("default_title").getString("title"));
+                    model.setSub_header(jsonObject1.getJSONObject("default_title").getString("sub_header"));
+                    model.setImage_url(jsonObject1.getJSONObject("default_title").getJSONObject("image").getString("image"));
+
+                    news_models.add(model);
                 }
+                binding.recview.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+                minecraftNewsAdapter = new MinecraftNewsAdapter(getApplicationContext(), news_models);
+                binding.recview.setAdapter(minecraftNewsAdapter);
+            } catch (JSONException e) {
+                //
             }
-        });*/
+        }, error -> Log.d("tag", "OnErrorResponse" + error.getMessage()));
+
+        queue.add(jsonObjectRequest);
     }
 
     public void BottomAppBar() {
@@ -138,30 +147,37 @@ public class MainActivity extends AppCompatActivity {
         btn_snapshot = bottomSheetDialog.findViewById(R.id.BS_MinecraftSnapshotBtn);
 
         btn_news.setOnClickListener(view -> {
+            base_models.clear();
+            news_models.clear();
+            ParseNews(NEWS_JSON);
             bottomSheetDialog.dismiss();
         });
 
         btn_bedrock.setOnClickListener(view -> {
-            models.clear();
-            ExtractJson(BEDROCK_JSON);
+            base_models.clear();
+            news_models.clear();
+            ParseChangelogs(BEDROCK_JSON);
             bottomSheetDialog.dismiss();
         });
 
         btn_beta_and_preview.setOnClickListener(view -> {
-            models.clear();
-            ExtractJson(BETA_JSON);
+            base_models.clear();
+            news_models.clear();
+            ParseChangelogs(BETA_JSON);
             bottomSheetDialog.dismiss();
         });
 
         btn_java.setOnClickListener(view -> {
-            models.clear();
-            ExtractJson(JAVA_JSON);
+            base_models.clear();
+            news_models.clear();
+            ParseChangelogs(JAVA_JSON);
             bottomSheetDialog.dismiss();
         });
 
         btn_snapshot.setOnClickListener(view -> {
-            models.clear();
-            ExtractJson(SNAPSHOT_JSON);
+            base_models.clear();
+            news_models.clear();
+            ParseChangelogs(SNAPSHOT_JSON);
             bottomSheetDialog.dismiss();
         });
     }
