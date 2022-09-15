b6720fa513fddd10abdc40abd99cd8cd8f0bc352
Ibragim Maltsagov <ibragim837.tab@gmail.com>
2022-09-15 11:46:20 +0200

    Добавлена функционал для парсинга и отображении статьи с чейнджлогом

--- app/src/main/java/com/mcbedrock/minecraftnews/ui/MarkdownActivity.java
+++ app/src/main/java/com/mcbedrock/minecraftnews/ui/MarkdownActivity.java
@@ -5,9 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.CenterCrop;
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
+import com.bumptech.glide.request.RequestOptions;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.mcbedrock.minecraftnews.R;
 import com.mcbedrock.minecraftnews.databinding.ActivityMarkdownBinding;
+import com.mcbedrock.minecraftnews.utils.ContentHelper;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import br.tiagohm.markdownview.css.InternalStyleSheet;
 import br.tiagohm.markdownview.css.styles.Github;
@@ -26,9 +40,34 @@ public class MarkdownActivity extends AppCompatActivity {
         Intent intent = this.getIntent();
         String URL = intent.getExtras().getString("URL");
 
-        InternalStyleSheet css = new Github();
-        binding.MarkdownView.addStyleSheet(css);
-        binding.MarkdownView.loadMarkdownFromUrl(URL);
+        RequestQueue queue = Volley.newRequestQueue(this);
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                Request.Method.GET,
+                URL,
+                null,
+                response -> {
+                    try {
+                        final String mimeType = "text/html";
+                        final String encoding = "UTF-8";
+                        String html = response.getString("body");
+                        String title = response.getString("title");
+                        binding.MarkdownView.loadDataWithBaseURL("", html, mimeType, encoding, "");
+
+                        RequestOptions requestOptions = new RequestOptions();
+                        requestOptions = requestOptions.transform(new CenterCrop());
+                        Glide.with(this)
+                                .load(ContentHelper.CONTENT + response.getJSONObject("image").getString("url"))
+                                .apply(requestOptions)
+                                .into(binding.ImageView);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                },
+                error -> {
+                    error.printStackTrace();
+                }
+        );
+        queue.add(jsonObjectRequest);
 
         binding.toolbar.setOnClickListener(view1 -> {
             onBackPressed();
